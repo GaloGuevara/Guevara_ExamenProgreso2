03024eeaf05813bd3b10c08373b47d7c301ae7a0
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-12-06 15:34:18 -0500

    \Ej.c Desarrollo de la función MayorNota e implementación en la función main, junto con pruebas de desarrollo finales y el código funciona correctamente

--- Ej.c
+++ Ej.c
@@ -1,4 +1,4 @@
-/*Cada semestre, a la materia de Programación se inscriben una cantidad constante de 23 alumnos. El semestre está dividido en 3 progresos. 
+/*Cada semestre, a la materia de Programación se inscriben una cantidad constante de 23 alumnos. El semestre está dividido en 3 progresos.
 Utilice los conceptos de Arreglos y Funciones para escribir un programa en lenguaje C que permita realizar los siguientes cálculos:
 1- Obtener el promedio de calificaciones de cada alumno durante el semeste.
 2- La nota promedio del grupo de estudiantes para cada progreso
@@ -7,13 +7,14 @@ Utilice los conceptos de Arreglos y Funciones para escribir un programa en lengu
 #include <time.h>
 #include <stdlib.h>
 
-void PromedioAlumno(int Notas[3][23]);
-void NotaPromedio(int Notas[3][23]);
-void MayorNota(int Notas[3][23]);
+void PromedioAlumno(int Notas[3][23], float promedioCurso[23]);
+void NotaPromedio(int Notas[3][23], float promedioCurso[23]);
+void MayorNota(int Notas[3][23], float promedioCurso[23]);
 
 int main(int argc, char const *argv[])
 {
     int Notas[3][23];
+    float promedioCurso[23];
 
     srand(time(NULL));
 
@@ -35,13 +36,14 @@ int main(int argc, char const *argv[])
     }
     puts("\n");
 
-    PromedioAlumno(Notas);
-    NotaPromedio(Notas);
+    PromedioAlumno(Notas, promedioCurso);
+    NotaPromedio(Notas, promedioCurso);
+    MayorNota(Notas, promedioCurso);
 
-    return 0;
+        return 0;
 }
 
-void PromedioAlumno(int Notas[3][23])
+void PromedioAlumno(int Notas[3][23], float promedioCurso[23])
 {
     int alumno = 0;
     puts("Ingrese el alumno que quiere ver el promedio: ");
@@ -56,11 +58,10 @@ void PromedioAlumno(int Notas[3][23])
     printf("El promedio del estudiante %d es %.2f\n", alumno, suma / 3);
 }
 
-void NotaPromedio(int Notas[3][23])
+void NotaPromedio(int Notas[3][23], float promedioCurso[23])
 {
     float sumaCurso = 0;
     float promedio = 0;
-    float promedioCurso[23];
 
     for (int alumno = 0; alumno < 23; alumno++)
     {
@@ -85,4 +86,21 @@ void NotaPromedio(int Notas[3][23])
     }
     puts("\n");
     printf("El promedio del curso es: %.2f\n", sumaCurso / 27);
+}
+
+void MayorNota(int Notas[3][23], float promedioCurso[23])
+{
+    float notaMax = 0;
+    int alumnoMax;
+
+    for (int i = 0; i < 23; i++)
+    {
+        if (promedioCurso[i] >= notaMax)
+        {
+            notaMax = promedioCurso[i];
+            alumnoMax = i+1;
+        }
+    }
+
+    printf("La mayor nota es %.2f correspondiente al alumno %d\n\n", notaMax, alumnoMax);
 }
